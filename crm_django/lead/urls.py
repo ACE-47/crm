4d6c7ba5ad90b34da48521f12053850468eab6d6
ACE-47 <haidermohammed66233@gmail.com>
2023-06-06 21:13:09 +0300

    init

--- crm_django/lead/urls.py
+++ crm_django/lead/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.LeadListView.as_view(),name='leads-list'),
     path('<int:pk>/', views.LeadDetailView.as_view(),name='lead-details'),
     path('<int:pk>/delete/', views.LeadDeleteView.as_view(),name='lead-delete'),
-    path('<int:pk>/edit/', views.edit_lead,name='edit-lead'),
+    path('<int:pk>/edit/', views.LeadUpdateView.as_view(),name='edit-lead'),
     path('<int:pk>/convert/', views.convert_to_client,name='lead-convert'),
-    path('add-lead/', views.add_lead,name='add-lead'),
+    path('add-lead/', views.LeadCreateView.as_view(),name='add-lead'),
 ]
--- crm_django/lead/views.py
+++ crm_django/lead/views.py
@@ -1,9 +1,12 @@
+from typing import Any, Dict
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.shortcuts import render, redirect, get_object_or_404 
-from django.views.generic import ListView, DetailView, DeleteView
+from django.views.generic import ListView, DetailView, DeleteView, UpdateView, CreateView
 from team.models import Team
 from .forms import AddLeadForm
 from .models import Lead
@@ -58,6 +61,53 @@ class LeadDeleteView(DeleteView):
     # def get(self, request, *args, **kwargs) -> HttpResponse:
     #     return self.post(request, *args, **kwargs)
 
+##########################################################
+
+class LeadUpdateView(UpdateView):
+    model = Lead
+    fields = ('name','email','description','priority','status')
+
+    template_name = 'lead/edit_lead.html'
+
+    @method_decorator(login_required)
+    def dispatch(self,  *args, **kwargs) :
+        return super().dispatch(*args, **kwargs)
+
+    def get_queryset(self):
+        queryset =  super(LeadUpdateView,self).get_queryset()
+        queryset = queryset.filter(created_by =self.request.user, pk = self.kwargs.get('pk'))
+        return queryset
+    
+    def get_success_url(self) -> str:
+        return reverse_lazy('leads-list')
+    
+
+class LeadCreateView(CreateView):
+    model = Lead
+    fields = ('name','email','description','priority','status')
+
+    template_name = 'lead/add_lead.html'
+
+    @method_decorator(login_required)
+    def dispatch(self,  *args, **kwargs) :
+        return super().dispatch(*args, **kwargs)
+    
+    def get_success_url(self) -> str:
+        return reverse_lazy('leads-list')
+    
+    def form_valid(self, form: BaseModelForm) :
+        team = Team.objects.filter(created_by = self.request.user)[0]
+        self.object = form.save(commit=False)
+        self.object.created_by = self.request.user
+        self.object.team = team
+        self.object.save()
+        return redirect(self.get_success_url())
+    
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['team'] = team = Team.objects.filter(created_by = self.request.user)[0]
+        return context
+
 # @login_required
 # def leads_list(request):
 #     leads = Lead.objects.filter(created_by = request.user,converted_to_client = False)
@@ -74,22 +124,23 @@ class LeadDeleteView(DeleteView):
 #     messages.success(request, 'The lead was successfuly deleted')
 #     return redirect('leads-list')
 
-@login_required
-def edit_lead(request,pk):
-    lead = get_object_or_404(Lead,created_by = request.user,pk = pk)
 
-    if request.method == 'POST':
-        form = AddLeadForm(request.POST,instance=lead)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'The changes were successfuly updated')
-            return redirect('leads-list')
-    else:
-        form = AddLeadForm(instance=lead)
+# @login_required
+# def edit_lead(request,pk):
+#     lead = get_object_or_404(Lead,created_by = request.user,pk = pk)
+
+#     if request.method == 'POST':
+#         form = AddLeadForm(request.POST,instance=lead)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'The changes were successfuly updated')
+#             return redirect('leads-list')
+#     else:
+#         form = AddLeadForm(instance=lead)
     
-    return render(request,'lead/edit_lead.html',{
-        'form':form
-    })
+#     return render(request,'lead/edit_lead.html',{
+#         'form':form
+#     })
 
 
 
