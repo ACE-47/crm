f59c2cc821ee288e1831def06e3d22413b6af169
ACE-47 <haidermohammed66233@gmail.com>
2023-06-07 19:59:55 +0300

    init

--- crm_django/team/urls.py
+++ crm_django/team/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:pk>/', views.team_detail,name='team-detail'),
     path('<int:pk>/edit/', views.edit_team,name='edit-team'),
 
 ]
--- crm_django/team/views.py
+++ crm_django/team/views.py
@@ -20,3 +20,10 @@ def edit_team(request, pk):
         'team':team,
         'form':form,
     })
+
+@login_required
+def team_detail(request, pk):
+    team = get_object_or_404(Team, created_by =request.user, pk=pk)
+    return render(request,'team/team_detail.html',{
+        'team':team
+    })
